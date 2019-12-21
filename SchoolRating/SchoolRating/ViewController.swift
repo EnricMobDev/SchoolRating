d4b2c3e1320468b02e370b3c504a47546f0eebe1
Enric Pou <epouvillanueva@gmail.com>
2019-12-22 00:58:45 +0100

    ADD project

---
+++ SchoolRating/SchoolRating/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SchoolRating
+//
+//  Created by Enric Pou Villanueva on 22/12/2019.
+//  Copyright © 2019 Enric Pou Villanueva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
